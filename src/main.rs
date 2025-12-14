007d571cef8eea19d6a4908a887437bb2e8e69f7
Skywalker8510 <Skywalker8510@gmail.com>
2025-12-14 04:44:24 -0600

    Display data!!!

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@
 )]
 
 extern crate alloc;
+
+use alloc::string::String;
 use embassy_executor::Spawner;
 use embedded_graphics::{
     mono_font::{MonoTextStyle, ascii::FONT_10X20},
@@ -26,7 +28,14 @@ use esp_println::println;
 use esp_radio::ble::controller::BleConnector;
 use mipidsi::interface::SpiInterface;
 use mipidsi::options::{Orientation, Rotation};
-use mipidsi::{Builder, TestImage, models::ST7789, options::ColorInversion};
+use mipidsi::{Builder, models::ST7789, options::ColorInversion};
+use bme680::{Bme680, SettingsBuilder, OversamplingSetting, IIRFilterSize, PowerMode, I2CAddress, FieldDataCondition};
+use embassy_time::Duration;
+use esp_hal::i2c::master::I2c;
+use esp_hal::{i2c::master::Config as I2CConfig, time::Rate};
+use embedded_hal::delay::DelayNs;
+use embassy_time::Timer;
+use crate::alloc::string::ToString;
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
@@ -68,6 +77,55 @@ async fn main(spawner: Spawner) -> ! {
         .init(&mut delay)
         .expect("Failed to initialize display");
 
+    //Configure BMP688
+    let i2c_config = I2CConfig::default().with_frequency(Rate::from_khz(100));
+    let i2c = I2c::new(peripherals.I2C0, i2c_config).unwrap()
+        .with_sda(peripherals.GPIO3)
+        .with_scl(peripherals.GPIO4);
+    let mut delayer = Delay::new();
+
+    let mut bmp688 = Bme680::init(i2c, &mut delayer, I2CAddress::Secondary).map_err(|e| {
+        log::error!("Error at bme680 init {e:?}");
+    }).unwrap();
+
+    let mut delay = Delay::new();
+
+    let settings = SettingsBuilder::new()
+        .with_humidity_oversampling(OversamplingSetting::OS2x)
+        .with_pressure_oversampling(OversamplingSetting::OS4x)
+        .with_temperature_oversampling(OversamplingSetting::OS8x)
+        .with_temperature_filter(IIRFilterSize::Size3)
+        .with_gas_measurement(Duration::from_millis(1500).into(), 320, 25)
+        .with_temperature_offset(-2.2)
+        .with_run_gas(true)
+        .build();
+
+    let profile_dur = bmp688.get_profile_dur(&settings.0).map_err(|e| {
+        log::error!("Unable to get profile dur {e:?}");
+    }).unwrap();
+    println!("Profile duration {:?}", profile_dur);
+    println!("Setting sensor settings");
+    bmp688.set_sensor_settings(&mut delayer, settings)
+        .map_err(|e| {
+            log::error!("Unable to apply sensor settings {e:?}");
+        }).unwrap();
+    println!("Setting forced power modes");
+    bmp688.set_sensor_mode(&mut delayer, PowerMode::ForcedMode)
+        .map_err(|e| {
+            log::error!("Unable to set sensor mode {e:?}");
+        }).unwrap();
+
+    let sensor_settings = bmp688.get_sensor_settings(settings.1);
+    println!("Sensor settings: {:?}", sensor_settings);
+
+    delay.delay_ms(5000u32);
+    let power_mode = bmp688.get_sensor_mode();
+    println!("Sensor power mode: {:?}", power_mode);
+    println!("Setting forced power modes");
+
+
+
+
     esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 73744);
     // COEX needs more RAM - so we've added some more
     esp_alloc::heap_allocator!(size: 64 * 1024);
@@ -84,58 +142,92 @@ async fn main(spawner: Spawner) -> ! {
     // TODO: Spawn some tasks
     let _ = spawner;
 
-    // Text
-    let char_w = 10;
-    let char_h = 20;
-    let text_style = MonoTextStyle::new(&FONT_10X20, Rgb565::WHITE);
-    let text = "Hello World ^_^;";
-    let mut text_x = display_w;
-    let text_y = displat_h / 2;
+    let _ = display.clear(Rgb565::BLACK);
 
-    // Alternating color
-    let colors = [Rgb565::RED, Rgb565::GREEN, Rgb565::BLUE];
+    let text_style_new = MonoTextStyle::new(&FONT_10X20, Rgb565::WHITE);
+    let text_style_old = MonoTextStyle::new(&FONT_10X20, Rgb565::BLACK);
 
-    // Clear the display initially
-    display.clear(colors[0]).unwrap();
+    //Static Display Text
+    let title = "Enviormental Monitor";
+    let title_x = 0;
+    let title_y = 15;
+    let static_temp_text = "Temperature: ";
+    let static_temp_x = 0;
+    let static_temp_y = 61;
+    let static_pressure_text = "Pressure: ";
+    let static_pressure_x = 0;
+    let static_pressure_y = 83;
+    let static_humidity_text = "Humidity: ";
+    let static_humidity_x = 0;
+    let static_humidity_y = 105;
+    let static_gas_text = "Air Quality: ";
+    let static_gas_x = 0;
+    let static_gas_y = 127;
+
+    Text::new(title, Point::new(title_x, title_y), text_style_new).draw(&mut display).unwrap();
+    let a = Text::new(static_temp_text, Point::new(static_temp_x, static_temp_y), text_style_new).draw(&mut display).unwrap();
+    let b = Text::new(static_pressure_text, Point::new(static_pressure_x, static_pressure_y), text_style_new).draw(&mut display).unwrap();
+    let c = Text::new(static_humidity_text, Point::new(static_humidity_x, static_humidity_y), text_style_new).draw(&mut display).unwrap();
+    let d = Text::new(static_gas_text, Point::new(static_gas_x, static_gas_y), text_style_new).draw(&mut display).unwrap();
+    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+    
+    let dyn_temp_x = a.x - 5;
+    let dyn_temp_y = static_temp_y;
+    let dyn_pressure_x = b.x - 5;
+    let dyn_pressure_y = static_pressure_y;
+    let dyn_humidity_x = c.x - 5;
+    let dyn_humidity_y = static_humidity_y;
+    let dyn_gas_x = d.x -5;
+    let dyn_gas_y = static_gas_y;
+
+    let mut dyn_temp_text: String;
+    let mut dyn_pressure_text: String;
+    let mut dyn_humidity_text: String;
+    let mut dyn_gas_text: String;
+
+    let mut old_dyn_temp_text: String = String::new();
+    let mut old_dyn_pressure_text: String = String::new();
+    let mut old_dyn_humidity_text: String = String::new();
+    let mut old_dyn_gas_text: String = String::new();
 
     // Turn on backlight
     backlight.set_high();
 
-    let mut last = esp_hal::time::Instant::now();
-    let mut counter = 0;
-    // let _ = display.clear(Rgb565::BLACK);
-    // let _ = TestImage::new().draw(&mut display);
     loop {
-        let elapsed = last.elapsed().as_secs() as f64;
-        if elapsed < 0.125 {
-            continue;
+        bmp688.set_sensor_mode(&mut delayer, PowerMode::ForcedMode)
+            .map_err(|e| {
+                log::error!("Unable to set sensor mode {e:?}");
+            }).unwrap();
+        println!("Retrieving sensor data");
+        let (data, state) = bmp688.get_sensor_data(&mut delayer).unwrap();
+        println!("Sensor Data {:?}", data);
+
+        if state == FieldDataCondition::NewData {
+            if old_dyn_temp_text != data.temperature_celsius().to_string() {
+                dyn_temp_text = data.temperature_celsius().to_string();
+                Text::new(&old_dyn_temp_text, Point::new(dyn_temp_x, dyn_temp_y), text_style_old).draw(&mut display).unwrap();
+                Text::new(&dyn_temp_text, Point::new(dyn_temp_x, dyn_temp_y), text_style_new).draw(&mut display).unwrap();
+                old_dyn_temp_text = dyn_temp_text;
+            }
+            if old_dyn_pressure_text != data.pressure_hpa().to_string() {
+                dyn_pressure_text = data.pressure_hpa().to_string();
+                Text::new(&old_dyn_pressure_text, Point::new(dyn_pressure_x, dyn_pressure_y), text_style_old).draw(&mut display).unwrap();
+                Text::new(&dyn_pressure_text, Point::new(dyn_pressure_x, dyn_pressure_y), text_style_new).draw(&mut display).unwrap();
+                old_dyn_pressure_text = dyn_pressure_text;
+            }
+            if old_dyn_humidity_text != data.humidity_percent().to_string() {
+                dyn_humidity_text = data.humidity_percent().to_string();
+                Text::new(&old_dyn_humidity_text, Point::new(dyn_humidity_x, dyn_humidity_y), text_style_old).draw(&mut display).unwrap();
+                Text::new(&dyn_humidity_text, Point::new(dyn_humidity_x, dyn_humidity_y), text_style_new).draw(&mut display).unwrap();
+                old_dyn_humidity_text = dyn_humidity_text;
+            }
+            if old_dyn_gas_text != data.gas_resistance_ohm().to_string() {
+                dyn_gas_text = data.gas_resistance_ohm().to_string();
+                Text::new(&old_dyn_gas_text, Point::new(dyn_gas_x, dyn_gas_y), text_style_old).draw(&mut display).unwrap();
+                Text::new(&dyn_gas_text, Point::new(dyn_gas_x, dyn_gas_y), text_style_new).draw(&mut display).unwrap();
+                old_dyn_gas_text = dyn_gas_text;
+            }
         }
-        last = esp_hal::time::Instant::now();
-        counter += 1;
-
-        // text_y -= char_h;
-        // text_y += char_h;
-
-        // // Fill the display with alternating colors every 8 frames
-        // display.clear(colors[(counter / 8) % colors.len()]).unwrap();
-        display.clear(Rgb565::BLACK).unwrap();
-
-        // Draw text
-        let right = Text::new(text, Point::new(text_x as i32, text_y as i32), text_style)
-            .draw(&mut display)
-            .unwrap();
-        println!("{:?}", text_x);
-        println!("{:?}", right.x);
-        text_x = if right.x <= 165 {
-            display_w
-        } else {
-            text_x - char_w
-        };
+        Timer::after(Duration::from_secs(5)).await;
     }
-
-    // loop {
-    //     Timer::after(Duration::from_secs(1)).await;
-    // }
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples/src/bin
 }
