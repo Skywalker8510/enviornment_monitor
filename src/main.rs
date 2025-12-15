c41a73dc4d0a188428abe03d7aae36b5adee01ef
Skywalker8510 <Skywalker8510@gmail.com>
2025-12-14 22:15:15 -0600

    wont compile without this?

--- src/main.rs
+++ src/main.rs
@@ -38,6 +38,8 @@ use esp_radio::ble::controller::BleConnector;
 use mipidsi::interface::SpiInterface;
 use mipidsi::options::{Orientation, Rotation};
 use mipidsi::{Builder, models::ST7789, options::ColorInversion};
+use esp_backtrace;
+
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
